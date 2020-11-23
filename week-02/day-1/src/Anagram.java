e553bbe4f13bddd45e59a12b7d8f38665acf6069
benczeg <benczegabor24@gmail.com>
2020-11-23 23:27:47 +0100

    Couldn't solve Unique, but proudly solved Angram for first try :)

---
+++ week-02/day-1/src/Anagram.java
@@ -0,0 +1,28 @@
+public class Anagram {
+    public static void main(String[] args) {
+        String word1 = "this";
+        String word2 = "shit";
+        System.out.println(anagram(word1, word2));
+    }
+
+    public static boolean anagram(String letters1, String letters2) {
+        if (letters1.length() == letters2.length()) {
+            int[] booleans = new int[letters1.length()];
+            for (int i = 0; i < letters1.length(); i++) {
+                for (int j = 0; j < letters2.length(); j++) {
+                    if (letters1.charAt(i) == letters2.charAt(j)) {
+                        booleans[i] = 1;
+                    }
+                }
+            }
+            boolean anagramFinal=true;
+            for (int i = 0; i < letters1.length(); i++) {
+                if (booleans[i] == 0){
+                    anagramFinal=false;
+                }
+            }
+            return anagramFinal;
+        }
+        return false;
+    }
+}
--- week-02/day-1/src/Unique.java
+++ week-02/day-1/src/Unique.java
@@ -3,29 +3,36 @@ import java.util.Arrays;
 //  Create a function that takes a list of numbers as a parameter
 //  Returns a list of numbers where every number in the list occurs only once
 public class Unique {
-    public static int[] unique(int[] numbers) {
-        int[] unikums = new int[numbers.length];
+    public static String[] unique(String[] numbers) {
+        String[] ratata = new String[8];
         int k = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            int alma = numbers[i];
-            for (int j = 0; j < unikums.length; j++) {
-                if (unikums[j] == 0 || k == unikums[j]) {
-                } else {
-                    unikums[i] = alma;
-                    k++;
+        for (int i = 0; i < 8; i++) {
+            boolean pikachu = true;
+            for (int j = i; 8 > j; j++) {
+                if (numbers[i].equals(numbers[j])) {
+                    pikachu = false;
                 }
             }
+            if (pikachu) {
+                ratata[k] = numbers[i];
+                k++;
+            }
+
+
         }
-        int[] cleared = new int[k];
+
+
+        String[] cleared = new String[k];
         for (int i = 0; i < k; i++) {
-            cleared[i] = unikums[i];
+            cleared[i] = ratata[i];
         }
         return cleared;
     }
 
     public static void main(String[] args) {
         int[] banana = {1, 11, 34, 11, 52, 61, 1, 34};
-        System.out.println(Arrays.toString(unique(banana)));
+        String[] apple = Arrays.toString(banana).split(",");
+        System.out.println(Arrays.toString(unique(apple)));
     }
 
 }
